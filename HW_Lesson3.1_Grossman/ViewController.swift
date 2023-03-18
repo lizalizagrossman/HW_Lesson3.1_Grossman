200f094c71b403f8d515e6a2b20aa6accedcf991
Liza Grossman <lisok.sam@gmail.com>
2023-03-18 12:23:52 +0200

    Initial Commit

---
+++ HW_Lesson3.1_Grossman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_Lesson3.1_Grossman
+//
+//  Created by Elizabeth on 18/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
